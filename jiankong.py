9ea1762b2ec67af6816bb112c31e915dc2f6e5bb
ZhenZhu-222 <18346506531@163.com>
2025-01-18 18:52:42 +0800

    Add files via upload

---
+++ jiankong.py
@@ -0,0 +1,9 @@
+class ragfile():
+    def __init__(self):
+        self.uploaded_file = None
+        self.memory = None
+        self.api = None
+        self.documents = None
+        self.texts = None
+ragtool = ragfile()
+
